da02452a1a3c93c21c95bfdfcbe238508617974d
garvit-codeverse <garvitcodeverse@gmail.com>
2026-07-30 23:52:09 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,10 @@
-# handlers
+# handlers/__init__.py – DEVILS WILL RISE EDITION 🔥
+from . import browse
+from . import payment
+from . import admin
+from . import user
+from . import deposit
+from . import otp
+from . import settings
+from . import session_manager
+from . import payment_checker
